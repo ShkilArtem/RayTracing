a2a2dfd124f5cc0f5a7450f515b1dcb88defc6b0
ShkilArtem <artem.e.shkil@gmail.com>
2025-07-14 16:16:37 +0200

    Random reflections

--- camera.h
+++ camera.h
@@ -9,6 +9,7 @@ class camera {
         double aspect_ration;
 	    int image_width;
         int sample_per_pixel = 10;
+        int max_depth = 10;
 
         void render(const hittable& world){
 
@@ -25,7 +26,7 @@ class camera {
 
                    for(int sample = 0; sample < sample_per_pixel; sample++){
                         ray r = get_ray(i,j);
-                        pixel_color += ray_color(r,world);
+                        pixel_color += ray_color(r, max_depth, world);
                    }
 
                     write_color(std::cout, pixel_sample_scale * pixel_color);
@@ -83,11 +84,16 @@ class camera {
             return vec3(random_double()-0.5, random_double()-0.5, 0);
         }
         
-        color ray_color(const ray& r, const hittable& world) const {
+        color ray_color(const ray& r, int depth, const hittable& world) const {
+            
+            if (depth <= 0) {
+                return color(0, 0, 0);
+            }
             hit_record rec;
             
-	        if(world.hit(r,interval (0, +infinity), rec)){
-		        return 0.5* (rec.normal + color(1,1,1));
+	        if(world.hit(r,interval (0.001, +infinity), rec)){
+                vec3 direction = random_on_hemisphere(rec.normal);
+                return 0.5 * ray_color(ray(rec.p, direction),depth - 1, world);
 	        }
 
 	        vec3 unit_direction = unit_vector(r.direction());
